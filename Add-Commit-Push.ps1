4a1be70aa1ef4750931eb5ae08f50c13964bdc0d
Julian Palacios Goerger <julian202@gmail.com>
2017-01-22 10:14:15 -0500

    update

--- Add-Commit-Push.ps1
+++ Add-Commit-Push.ps1
@@ -1,4 +1,4 @@
 git add .
 git commit -m "update"
-git push https://julian202:Pimpim08@github.com/julian202/convert-formula.git
+git push origin https://julian202:Pimpim08@github.com/julian202/convert-formula.git
 Read-Host -Prompt “Press Enter to exit”
