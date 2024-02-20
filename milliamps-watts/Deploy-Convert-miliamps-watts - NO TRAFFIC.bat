7a518602bad73aaca06d6daca9bf4ae7e8f1f929
Julian <julian202@gmail.com>
2024-02-20 08:55:36 -0500

    Added milliamps-watts to git index.

---
+++ milliamps-watts/Deploy-Convert-miliamps-watts - NO TRAFFIC.bat
@@ -0,0 +1,9 @@
+ECHO OFF
+CLS
+SET PATH=C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin;%PATH%;
+cd C:\Program Files (x86)\Google\Cloud SDK
+ECHO Welcome to the Google Cloud SDK! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
+gcloud app deploy "C:\Users\julia\OneDrive\App Engine and More\milliamps-watts\FilesToDeploy\app.yaml" --project milliamps-watts --no-promote --verbosity info
+pause
