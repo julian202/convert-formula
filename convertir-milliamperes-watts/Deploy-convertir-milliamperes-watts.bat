8c3193a0d8fee508cf94ed67d1277328f81fbc2a
Julian <julian202@gmail.com>
2024-02-21 18:19:07 -0500

    Add project to git index: convertir-milliamperes-watts

---
+++ convertir-milliamperes-watts/Deploy-convertir-milliamperes-watts.bat
@@ -0,0 +1,10 @@
+ECHO OFF
+CLS
+SET PATH=C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin;%PATH%;
+cd C:\Program Files (x86)\Google\Cloud SDK
+ECHO Welcome to the Google Cloud SDK! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
+gcloud app deploy "C:\Users\julia\OneDrive\App Engine and More\convertir-milliamperes-watts\FilesToDeploy\app.yaml" --project convertir-milliamperes-watts --verbosity info
+
+
