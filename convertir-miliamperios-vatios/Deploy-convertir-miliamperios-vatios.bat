c9906691d09fdcdacd85e53989806b532f6fd3e5
Julian <julian202@gmail.com>
2024-02-28 22:18:29 -0500

    Finished moving milliamp watts in different languages to convert-formula.

--- convertir-miliamperios-vatios/Deploy-convertir-miliamperios-vatios.bat
+++ convertir-miliamperios-vatios/Deploy-convertir-miliamperios-vatios.bat
@@ -5,5 +5,5 @@ cd C:\Program Files (x86)\Google\Cloud SDK
 ECHO Welcome to the Google Cloud SDK! Run "gcloud -h" to get the list of available commands.
 ECHO ---
 ECHO ON
-gcloud app deploy "C:\Users\julia\OneDrive\App Engine and More\convertir-miliamperios-vatios\FilesToDeploy\app.yaml" --project convertir-miliamperios-vatios --verbosity info
+gcloud app deploy "C:\Users\julia\OneDrive\App Engine and More\convertir-miliamperios-vatios\my-nodejs-service\app.yaml" --project convertir-miliamperios-vatios --verbosity info
 
